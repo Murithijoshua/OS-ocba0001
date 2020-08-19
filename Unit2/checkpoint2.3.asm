5325b35c46f4073b4d8924a118a61cab79bf0b92
Sherica Fabienne Ocbania <ocba0001@flinders.edu.au>
2020-08-19 10:22:58 +0930

    Checkpoint 2.3 Commit

---
+++ Unit2/checkpoint2.3.asm
@@ -0,0 +1,573 @@
+// XMega65 Kernal Development Template
+// Each function of the kernal is a no-args function
+// The functions are placed in the SYSCALLS table surrounded by JMP and NOP
+  .file [name="checkpoint2.3.bin", type="bin", segments="XMega65Bin"]
+.segmentdef XMega65Bin [segments="Syscall, Code, Data, Stack, Zeropage"]
+.segmentdef Syscall [start=$8000, max=$81ff]
+.segmentdef Code [start=$8200, min=$8200, max=$bdff]
+.segmentdef Data [startAfter="Code", min=$8200, max=$bdff]
+.segmentdef Stack [min=$be00, max=$beff, fill]
+.segmentdef Zeropage [min=$bf00, max=$bfff, fill]
+  // Some definitions of addresses and special values that this program uses
+  .label RASTER = $d012
+  .label VIC_MEMORY = $d018
+  .label SCREEN = $400
+  .label BGCOL = $d021
+  .label COLS = $d800
+  .const BLACK = 0
+  .const WHITE = 1
+  .const JMP = $4c
+  .const NOP = $ea
+.segment Code
+main: {
+    rts
+}
+cpukil: {
+    jsr exit_hypervisor
+    rts
+}
+exit_hypervisor: {
+    // Trigger exit from Hypervisor Mode
+    lda #1
+    sta $d67f
+    rts
+}
+reserved: {
+    jsr exit_hypervisor
+    rts
+}
+vf011wr: {
+    jsr exit_hypervisor
+    rts
+}
+vf011rd: {
+    jsr exit_hypervisor
+    rts
+}
+alttabkey: {
+    jsr exit_hypervisor
+    rts
+}
+restorkey: {
+    jsr exit_hypervisor
+    rts
+}
+pagfault: {
+    jsr exit_hypervisor
+    rts
+}
+reset: {
+    .label sc = 4
+    .label msg = 2
+    // Initialize screen memory and select correct font
+    lda #$14
+    sta VIC_MEMORY
+    ldx #' '
+    lda #<SCREEN
+    sta.z memset.str
+    lda #>SCREEN
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    ldx #WHITE
+    lda #<COLS
+    sta.z memset.str
+    lda #>COLS
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    lda #<SCREEN+$28
+    sta.z sc
+    lda #>SCREEN+$28
+    sta.z sc+1
+    lda #<MESSAGE
+    sta.z msg
+    lda #>MESSAGE
+    sta.z msg+1
+  // A simple copy routine to copy the string
+  __b1:
+    ldy #0
+    lda (msg),y
+    cmp #0
+    bne __b2
+  __b3:
+    lda #$36
+    cmp RASTER
+    beq __b4
+    lda #$42
+    cmp RASTER
+    beq __b4
+    lda #BLACK
+    sta BGCOL
+    jmp __b3
+  __b4:
+    lda #WHITE
+    sta BGCOL
+    jmp __b3
+  __b2:
+    ldy #0
+    lda (msg),y
+    sta (sc),y
+    inc.z sc
+    bne !+
+    inc.z sc+1
+  !:
+    inc.z msg
+    bne !+
+    inc.z msg+1
+  !:
+    jmp __b1
+}
+// Copies the character c (an unsigned char) to the first num characters of the object pointed to by the argument str.
+// memset(void* zeropage(4) str, byte register(X) c, word zeropage(2) num)
+memset: {
+    .label end = 2
+    .label dst = 4
+    .label num = 2
+    .label str = 4
+    lda.z num
+    bne !+
+    lda.z num+1
+    beq __breturn
+  !:
+    lda.z end
+    clc
+    adc.z str
+    sta.z end
+    lda.z end+1
+    adc.z str+1
+    sta.z end+1
+  __b2:
+    lda.z dst+1
+    cmp.z end+1
+    bne __b3
+    lda.z dst
+    cmp.z end
+    bne __b3
+  __breturn:
+    rts
+  __b3:
+    txa
+    ldy #0
+    sta (dst),y
+    inc.z dst
+    bne !+
+    inc.z dst+1
+  !:
+    jmp __b2
+}
+syscall3F: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3E: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3D: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3C: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3B: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3A: {
+    jsr exit_hypervisor
+    rts
+}
+syscall39: {
+    jsr exit_hypervisor
+    rts
+}
+syscall38: {
+    jsr exit_hypervisor
+    rts
+}
+syscall37: {
+    jsr exit_hypervisor
+    rts
+}
+syscall36: {
+    jsr exit_hypervisor
+    rts
+}
+syscall35: {
+    jsr exit_hypervisor
+    rts
+}
+syscall34: {
+    jsr exit_hypervisor
+    rts
+}
+syscall33: {
+    jsr exit_hypervisor
+    rts
+}
+syscall32: {
+    jsr exit_hypervisor
+    rts
+}
+syscall31: {
+    jsr exit_hypervisor
+    rts
+}
+syscall30: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2F: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2E: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2D: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2C: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2B: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2A: {
+    jsr exit_hypervisor
+    rts
+}
+syscall29: {
+    jsr exit_hypervisor
+    rts
+}
+syscall28: {
+    jsr exit_hypervisor
+    rts
+}
+syscall27: {
+    jsr exit_hypervisor
+    rts
+}
+syscall26: {
+    jsr exit_hypervisor
+    rts
+}
+syscall25: {
+    jsr exit_hypervisor
+    rts
+}
+syscall24: {
+    jsr exit_hypervisor
+    rts
+}
+syscall23: {
+    jsr exit_hypervisor
+    rts
+}
+syscall22: {
+    jsr exit_hypervisor
+    rts
+}
+syscall21: {
+    jsr exit_hypervisor
+    rts
+}
+syscall20: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1F: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1E: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1D: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1C: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1B: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1A: {
+    jsr exit_hypervisor
+    rts
+}
+syscall19: {
+    jsr exit_hypervisor
+    rts
+}
+syscall18: {
+    jsr exit_hypervisor
+    rts
+}
+syscall17: {
+    jsr exit_hypervisor
+    rts
+}
+syscall16: {
+    jsr exit_hypervisor
+    rts
+}
+syscall15: {
+    jsr exit_hypervisor
+    rts
+}
+syscall14: {
+    jsr exit_hypervisor
+    rts
+}
+syscall13: {
+    jsr exit_hypervisor
+    rts
+}
+securexit: {
+    jsr exit_hypervisor
+    rts
+}
+securentr: {
+    jsr exit_hypervisor
+    rts
+}
+syscall10: {
+    jsr exit_hypervisor
+    rts
+}
+syscallF: {
+    jsr exit_hypervisor
+    rts
+}
+syscallE: {
+    jsr exit_hypervisor
+    rts
+}
+syscallD: {
+    jsr exit_hypervisor
+    rts
+}
+syscallC: {
+    jsr exit_hypervisor
+    rts
+}
+syscallB: {
+    jsr exit_hypervisor
+    rts
+}
+syscallA: {
+    jsr exit_hypervisor
+    rts
+}
+syscall9: {
+    jsr exit_hypervisor
+    rts
+}
+syscall8: {
+    jsr exit_hypervisor
+    rts
+}
+syscall7: {
+    jsr exit_hypervisor
+    rts
+}
+syscall6: {
+    jsr exit_hypervisor
+    rts
+}
+syscall5: {
+    jsr exit_hypervisor
+    rts
+}
+syscall4: {
+    jsr exit_hypervisor
+    rts
+}
+syscall3: {
+    jsr exit_hypervisor
+    rts
+}
+syscall2: {
+    jsr exit_hypervisor
+    rts
+}
+syscall1: {
+    jsr exit_hypervisor
+    rts
+}
+// Here are a couple sample SYSCALL handlers that just display a character on the screen 
+syscall0: {
+    jsr exit_hypervisor
+    rts
+}
+.segment Data
+  // Some text to display
+  MESSAGE: .text "Checkpoint 2.3 by ocba0001"
+  .byte 0
+.segment Syscall
+  SYSCALLS: .byte JMP
+  .word syscall0
+  .byte NOP, JMP
+  .word syscall1
+  .byte NOP, JMP
+  .word syscall2
+  .byte NOP, JMP
+  .word syscall3
+  .byte NOP, JMP
+  .word syscall4
+  .byte NOP, JMP
+  .word syscall5
+  .byte NOP, JMP
+  .word syscall6
+  .byte NOP, JMP
+  .word syscall7
+  .byte NOP, JMP
+  .word syscall8
+  .byte NOP, JMP
+  .word syscall9
+  .byte NOP, JMP
+  .word syscallA
+  .byte NOP, JMP
+  .word syscallB
+  .byte NOP, JMP
+  .word syscallC
+  .byte NOP, JMP
+  .word syscallD
+  .byte NOP, JMP
+  .word syscallE
+  .byte NOP, JMP
+  .word syscallF
+  .byte NOP, JMP
+  .word syscall10
+  .byte NOP, JMP
+  .word securentr
+  .byte NOP, JMP
+  .word securexit
+  .byte NOP, JMP
+  .word syscall13
+  .byte NOP, JMP
+  .word syscall14
+  .byte NOP, JMP
+  .word syscall15
+  .byte NOP, JMP
+  .word syscall16
+  .byte NOP, JMP
+  .word syscall17
+  .byte NOP, JMP
+  .word syscall18
+  .byte NOP, JMP
+  .word syscall19
+  .byte NOP, JMP
+  .word syscall1A
+  .byte NOP, JMP
+  .word syscall1B
+  .byte NOP, JMP
+  .word syscall1C
+  .byte NOP, JMP
+  .word syscall1D
+  .byte NOP, JMP
+  .word syscall1E
+  .byte NOP, JMP
+  .word syscall1F
+  .byte NOP, JMP
+  .word syscall20
+  .byte NOP, JMP
+  .word syscall21
+  .byte NOP, JMP
+  .word syscall22
+  .byte NOP, JMP
+  .word syscall23
+  .byte NOP, JMP
+  .word syscall24
+  .byte NOP, JMP
+  .word syscall25
+  .byte NOP, JMP
+  .word syscall26
+  .byte NOP, JMP
+  .word syscall27
+  .byte NOP, JMP
+  .word syscall28
+  .byte NOP, JMP
+  .word syscall29
+  .byte NOP, JMP
+  .word syscall2A
+  .byte NOP, JMP
+  .word syscall2B
+  .byte NOP, JMP
+  .word syscall2C
+  .byte NOP, JMP
+  .word syscall2D
+  .byte NOP, JMP
+  .word syscall2E
+  .byte NOP, JMP
+  .word syscall2F
+  .byte NOP, JMP
+  .word syscall30
+  .byte NOP, JMP
+  .word syscall31
+  .byte NOP, JMP
+  .word syscall32
+  .byte NOP, JMP
+  .word syscall33
+  .byte NOP, JMP
+  .word syscall34
+  .byte NOP, JMP
+  .word syscall35
+  .byte NOP, JMP
+  .word syscall36
+  .byte NOP, JMP
+  .word syscall37
+  .byte NOP, JMP
+  .word syscall38
+  .byte NOP, JMP
+  .word syscall39
+  .byte NOP, JMP
+  .word syscall3A
+  .byte NOP, JMP
+  .word syscall3B
+  .byte NOP, JMP
+  .word syscall3C
+  .byte NOP, JMP
+  .word syscall3D
+  .byte NOP, JMP
+  .word syscall3E
+  .byte NOP, JMP
+  .word syscall3F
+  .byte NOP
+  .align $100
+  TRAPS: .byte JMP
+  .word reset
+  .byte NOP, JMP
+  .word pagfault
+  .byte NOP, JMP
+  .word restorkey
+  .byte NOP, JMP
+  .word alttabkey
+  .byte NOP, JMP
+  .word vf011rd
+  .byte NOP, JMP
+  .word vf011wr
+  .byte NOP, JMP
+  .word reserved
+  .byte NOP, JMP
+  .word cpukil
+  .byte NOP
